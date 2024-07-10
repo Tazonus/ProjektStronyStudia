3b5ffb93a302c6f36e8d4d68b6a9202d5e7e0734
Tazonus <Filip.herbik@gmail.com>
2024-07-10 22:25:57 +0200

    Fix Matplotlib crashin still crashes but not so often

--- server.py
+++ server.py
@@ -23,23 +23,27 @@ def math():
 @app.route('/send-message', methods=['POST'])
 def send_message():
     message = request.form['message']
-    print("Message: {message}")
-    image = generate_letter_histogram(message)
+    print(f"Message: {message}")
+    generate_letter_histogram(message)
     return send_file("plot.png")
 
 def generate_letter_histogram(word):
-    word = word.lower().replace(" ", "")
-    letter_counts = Counter(word)
-    letters = list(letter_counts.keys())
-    counts = list(letter_counts.values())
+    try:
+        word = word.lower().replace(" ", "")
+        letter_counts = Counter(word)
+        letters = list(letter_counts.keys())
+        counts = list(letter_counts.values())
 
-    plt.figure(figsize=(10, 5))
-    plt.bar(letters, counts, color='#218c74')
-    plt.title(f'Liczba wystąpień liter w słowie "{word}"')
-    plt.xlabel('Litery')
-    plt.ylabel('Liczba wystąpień')
-    plt.savefig('plot.png')
-    plt.close()
+        plt.bar(letters, counts, color='#218c74')
+        plt.title('częstotliwość liter w podanym zdaniu')
+        plt.xlabel('Litery')
+        plt.ylabel('Liczba wystąpień')
+        plt.savefig('plot.png')
+        plt.close()
+    except:
+        index()
+    #matplot gui is problematic in servers
+    #this try except is saving it from crashing(mostly)
 
 
 if __name__ == '__main__':
