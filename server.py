a1282a44326212d1781615f47af11abec5f12303
Tazonus <Filip.herbik@gmail.com>
2024-07-10 20:52:09 +0200

    ADD Placeholders for more people as well as general grahpic update

--- server.py
+++ server.py
@@ -1,9 +1,11 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template
+import matplotlib.pyplot as plt
+from collections import Counter
 
 app = Flask(__name__)
 
 @app.route('/')
-def index():
+def index(word = ''):
     return render_template('home.html')
 @app.route('/about')
 def about():
@@ -14,15 +16,6 @@ def project():
 @app.route('/math')
 def math():
     return render_template('math.html')
-
-@app.route('/send-message', methods=['POST'])
-def send_message():
-    name = request.form['name']
-    email = request.form['email']
-    message = request.form['message']
-    # Tutaj można dodać kod do przetwarzania danych formularza, np. zapisywanie do bazy danych lub wysyłanie emaila
-    print(f"Name: {name}, Email: {email}, Message: {message}")
-    return 'Wiadomość została wysłana pomyślnie!'
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0')
