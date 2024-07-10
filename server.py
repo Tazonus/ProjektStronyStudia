a6365dedbd3d413e4a9c60eb1687013ddc71d631
Tazonus <Filip.herbik@gmail.com>
2024-07-10 17:20:59 +0200

    ADD Math and forms at the moment as god Howard said, it just works

--- server.py
+++ server.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
@@ -14,6 +14,15 @@ def project():
 @app.route('/math')
 def math():
     return render_template('math.html')
+
+@app.route('/send-message', methods=['POST'])
+def send_message():
+    name = request.form['name']
+    email = request.form['email']
+    message = request.form['message']
+    # Tutaj można dodać kod do przetwarzania danych formularza, np. zapisywanie do bazy danych lub wysyłanie emaila
+    print(f"Name: {name}, Email: {email}, Message: {message}")
+    return 'Wiadomość została wysłana pomyślnie!'
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0')
